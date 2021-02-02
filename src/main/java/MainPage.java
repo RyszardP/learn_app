f898a14dd70d269b2ae8f14b0e8c3578391ceb98
Rysard Poplavskij <rpoplavskij@gmail.com>
2021-02-02 14:04:14 +0300

    Commit

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-
 import java.util.concurrent.TimeUnit;
 
 
